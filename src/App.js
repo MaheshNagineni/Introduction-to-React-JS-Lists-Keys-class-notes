87cef285b97d33b8169799e04a51aaf5780e5ad6
MaheshNagineni <maheshb061@gmail.com>
2023-04-07 12:13:20 +0000

    classnotes

---
+++ src/App.js
@@ -0,0 +1,44 @@
+// #creating user Details compoents
+import UserProfile from './components/UserProfile'
+
+import './App.css'
+
+const userDetailsList = [
+  {
+    uniqueNo: 1,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Mahesh Babu',
+    role: 'Software Developer',
+  },
+  {
+    uniqueNo: 2,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
+    name: 'Mahesh Babu',
+    role: 'Software Developer',
+  },
+  {
+    uniqueNo: 3,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
+    name: 'Mahesh Babu',
+    role: 'Software Developer',
+  },
+  {
+    uniqueNo: 4,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/devon-lane-img.png',
+    name: 'Mahesh Babu',
+    role: 'Software Developer',
+  },
+]
+
+const App = () => (
+  <div className="list-container">
+    <h1 className="title">Users Lists</h1>
+    <ul>
+      {userDetailsList.map(eachItem => (
+        <UserProfile userDetails={eachItem} key={eachItem.uniqueNo} />
+      ))}
+    </ul>
+  </div>
+)
+
+export default App
